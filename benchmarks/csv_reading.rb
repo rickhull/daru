6c2c4a3299a8f414132ff96888e0f92bb1b98340
Rick Hull <rick.hull@gmail.com>
2017-02-17 04:39:41 +0000

    seed much larger tempfiles with TradeoffData.csv
    - ingest a long file at 999x
    - ingest a wide file at 999x
    - daru takes 3-4x longer than ruby for long
             and 5-6x longer than ruby for wide

--- benchmarks/csv_reading.rb
+++ benchmarks/csv_reading.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 # Date - 28 june 2016. daru version - 0.1.3.1
 # Compare speed of Ruby stdlib CSV and DataFrame.from_csv.
 
@@ -5,14 +7,26 @@ require 'benchmark'
 require 'csv'
 require 'daru'
 
-Benchmark.bm do |x|
-  x.report("Ruby CSV") do
-    CSV.read("TradeoffData.csv")
-  end
+csv_contents = File.read(File.join(__dir__, 'TradeoffData.csv'))
 
-  x.report("DataFrame.from_csv") do
-    Daru::DataFrame.from_csv("TradeoffData.csv")
-  end
+long_csv_file = Tempfile.new('long_csv')
+long_csv_file.write csv_contents * 999
+long_csv_file.close
+puts `wc -l #{long_csv_file.path}`
+
+wide_csv_file = Tempfile.new('wide_csv')
+csv_contents.split("\n").each { |line|
+  wide_csv_file.write(("#{line}," * 999) + "\n")
+}
+wide_csv_file.close
+puts `wc -l #{wide_csv_file.path}`
+
+Benchmark.bm do |x|
+  x.report("Ruby CSV (long)") { CSV.read(long_csv_file.path) }
+  x.report("Ruby CSV (wide)") { CSV.read(wide_csv_file.path) }
+
+  x.report("Daru CSV (long)") { Daru::DataFrame.from_csv(long_csv_file.path) }
+  x.report("Daru CSV (wide)") { Daru::DataFrame.from_csv(wide_csv_file.path) }
 end
 
 # FIXME: Improve this. It's 4 times slower than Ruby CSV reading!!
